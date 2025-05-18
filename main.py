0456ac3c266848b1c09837527b56a77e93aed22d
norashigoto <61675817+norashigoto@users.noreply.github.com>
2025-05-18 18:56:07 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import dash
 from dash import html
 
 # Dashアプリケーションの作成
-app_ds = dash.Dash(__name__)
-server = app_ds.server  # gunicornが利用するFlaskサーバ
+app = dash.Dash(__name__)
+server = app.server  # gunicornが利用するFlaskサーバ
 
 # アプリのレイアウト定義
-app_ds.layout = html.Div([
+app.layout = html.Div([
     html.H1("Render + Dash アプリケーション"),
     html.P("これはRender上で動作するDashサンプルです。")
 ])
 
 # ローカル実行用
 if __name__ == "__main__":
-    app_ds.run(debug=True)
+    app.run(debug=True)
